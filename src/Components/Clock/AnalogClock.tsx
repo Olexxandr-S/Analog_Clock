1b8814a0a621c6729e8f350b249f57879bc16cc1
OlexxandrS <94759574+OlexxandrS@users.noreply.github.com>
2023-04-01 17:35:02 +0300

    binary-clock_theme

--- src/Components/Clock/AnalogClock.tsx
+++ src/Components/Clock/AnalogClock.tsx
@@ -3,18 +3,18 @@ import React from "react";
 export default function AnalogClock() {
   return (
     <>
-      <span className="clock__twelve">12</span>
-      <span className="clock__three">3</span>
-      <span className="clock__six">6</span>
-      <span className="clock__nine">9</span>
+      <span className="clock__twelve">1100</span>
+      <span className="clock__three">11</span>
+      <span className="clock__six">110</span>
+      <span className="clock__nine">1001</span>
       <span className="clock__one">1</span>
-      <span className="clock__two">2</span>
-      <span className="clock__four">4</span>
-      <span className="clock__five">5</span>
-      <span className="clock__seven">7</span>
-      <span className="clock__eight">8</span>
-      <span className="clock__ten">10</span>
-      <span className="clock__eleven">11</span>
+      <span className="clock__two">10</span>
+      <span className="clock__four">100</span>
+      <span className="clock__five">101</span>
+      <span className="clock__seven">111</span>
+      <span className="clock__eight">1000</span>
+      <span className="clock__ten">1010</span>
+      <span className="clock__eleven">1011</span>
 
       <div className="clock__rounder"></div>
       <div className="clock__hour" id="clock-hour"></div>
