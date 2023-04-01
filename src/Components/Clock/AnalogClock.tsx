83a6622c252443f66f1666583380c8136a8391f5
OlexxandrS <94759574+OlexxandrS@users.noreply.github.com>
2023-04-01 17:11:12 +0300

    123-clock_theme

--- src/Components/Clock/AnalogClock.tsx
+++ src/Components/Clock/AnalogClock.tsx
@@ -3,18 +3,18 @@ import React from "react";
 export default function AnalogClock() {
   return (
     <>
-      <span className="clock__twelve">12</span>
-      <span className="clock__three">3</span>
-      <span className="clock__six">6</span>
-      <span className="clock__nine">9</span>
-      <span className="clock__one">1</span>
-      <span className="clock__two">2</span>
-      <span className="clock__four">4</span>
-      <span className="clock__five">5</span>
-      <span className="clock__seven">7</span>
-      <span className="clock__eight">8</span>
-      <span className="clock__ten">10</span>
-      <span className="clock__eleven">11</span>
+      <span className="clock__twelve">1!&#183;2!&#183;3!</span>
+      <span className="clock__three">(-1+2)&#183;3</span>
+      <span className="clock__six">1+2+3</span>
+      <span className="clock__nine">1+2&sup3;</span>
+      <span className="clock__one">1+2/3</span>
+      <span className="clock__two">1-2+3</span>
+      <span className="clock__four">1&sup2;+3</span>
+      <span className="clock__five">1&#183;2+3</span>
+      <span className="clock__seven">1+2&#183;3</span>
+      <span className="clock__eight">1&#183;2&sup3;</span>
+      <span className="clock__ten">&#8739;12-&radic;3&#8739;</span>
+      <span className="clock__eleven">-1+2&#183;3!</span>
 
       <div className="clock__rounder"></div>
       <div className="clock__hour" id="clock-hour"></div>
